af7952f992da27024ae5c9e4244d274843e83866
saucedontsauce <98565306+saucedontsauce@users.noreply.github.com>
2024-02-12 18:37:26 +0000

    free code camp course

---
+++ myApp.js
@@ -0,0 +1,39 @@
+let express = require('express');
+let app = express();
+app.use("/public",express.static(__dirname + "/public"));
+app.get('/', (req,res)=>{res.sendFile(__dirname+"/views/index.html")})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
